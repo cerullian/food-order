097c1ba004f0d8e75b508b6a7701b8e179e4a36f
“dpatricio” <dpatricio@ubiwhere.com>
2023-04-06 14:04:12 +0100

    added cart modal state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,20 +6,28 @@ const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <CartItem key={item.id} id={item.id} name={item.name} amount={item.amount} price={item.price} />
+        <CartItem
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+        />
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,14 @@
 import mealsImage from "../../assets/meals.jpg";
-import React, { useState } from "react";
+import React from "react";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-import Modal from "../UI/Modal/Modal";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table Full of delicious food!" />
